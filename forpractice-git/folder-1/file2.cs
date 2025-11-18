e1b0afada8c8e67f1f43fe21abe752f8b3ec41c3
Kammal Kumar <kumarkamalbk@gmail.com>
2025-11-18 18:04:40 +0530

    troubleshppt error

---
+++ forpractice-git/folder-1/file2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forpractice_git.NewFolder
+    //ho
+{
+    internal class file2
+    {
+        Console.WriteLine("Add in branch 2");
+    }
+}
---
+++ forpractice-git/folder2/branch-2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forpractice_git.folder2
+{
+    internal class branch_2
+    {
+    }
+}
