701bdeb322140049e309cbb8c8a345cd9479bb5e
Kammal Kumar <kumarkamalbk@gmail.com>
2025-11-19 13:38:24 +0530

    committed successfully

--- forpractice-git/folder-1/fileforbranch-1.cs
+++ forpractice-git/folder-1/fileforbranch-1.cs
@@ -15,9 +15,13 @@ namespace forpractice_git
 
             int a = 10;
             int b = 20;
+            int c = 30;
+            int d = 50;
             int sum = a + b;
+            int sum1 = a + b + c + d;
 
             Console.WriteLine("Sum of a and b = " + sum);
+            Console.WriteLine("sum of a,b,c,d= " + sum1);
         }
     }
 }
