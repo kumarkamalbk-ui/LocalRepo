5a1085e1f2394a482e13a8e042ecfe2287473ccc
Kammal Kumar <kumarkamalbk@gmail.com>
2025-11-19 13:22:39 +0530

    successfull

--- forpractice-git/folder-1/fileforbranch-1.cs
+++ forpractice-git/folder-1/fileforbranch-1.cs
@@ -8,5 +8,16 @@ namespace forpractice_git
 {
     internal class fileforbranch_1
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a small program inside fileforbranch_1");
+            Console.WriteLine("Welcome to Git practice!");
+
+            int a = 10;
+            int b = 20;
+            int sum = a + b;
+
+            Console.WriteLine("Sum of a and b = " + sum);
+        }
     }
 }
